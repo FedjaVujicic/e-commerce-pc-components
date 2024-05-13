a454abb3a1f2450946c2c224fb6238d41d59eb2b
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-05-13 13:32:59 +0200

    Add error handling to monitor service

--- ComponentShopClient/src/app/shared/monitor.service.ts
+++ ComponentShopClient/src/app/shared/monitor.service.ts
@@ -22,37 +22,63 @@ export class MonitorService {
   }
 
   getMonitors() {
-    return this.http.get(this.url).subscribe(monitor => {
-      this.monitorList = monitor as Array<Monitor>;
+    return this.http.get(this.url).subscribe({
+      next: res => {
+        this.monitorList = res as Array<Monitor>;
+      },
+      error: err => {
+        console.log(err);
+      }
     });
   }
 
   getMonitor(id: number) {
-    return this.http.get(this.url + `/${id}`).subscribe(monitor => {
-      this.formData = monitor as Monitor;
-    });
+    return this.http.get(this.url + `/${id}`).subscribe(
+      {
+        next: res => {
+          this.formData = res as Monitor;
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
   }
 
   postMonitor() {
-    return this.http.post(this.url, this.formData).subscribe(monitor => {
-      this.monitorList = monitor as Array<Monitor>;
-      this.resetForm();
-      this.toastr.success("Added");
+    return this.http.post(this.url, this.formData).subscribe({
+      next: res => {
+        this.monitorList = res as Array<Monitor>;
+        this.resetForm();
+        this.toastr.success("Added");
+      },
+      error: err => {
+        console.log(err);
+      }
     });
   }
 
   deleteMonitor(id: number) {
-    return this.http.delete(this.url + `/${id}`).subscribe(monitor => {
-      this.monitorList = monitor as Array<Monitor>;
-      this.toastr.success("Deleted");
+    return this.http.delete(this.url + `/${id}`).subscribe({
+      next: res => {
+        this.monitorList = res as Array<Monitor>;
+        this.toastr.success("Deleted");
+      },
+      error: err => {
+        console.log(err);
+      }
     });
   }
 
   putMonitor(id: number, monitor: Monitor) {
-    return this.http.put(this.url + `/${id}`, monitor).subscribe(monitor => {
-      this.monitorList = monitor as Array<Monitor>;
-      this.resetForm();
-      this.toastr.success("Updated");
+    return this.http.put(this.url + `/${id}`, monitor).subscribe({
+      next: res => {
+        this.monitorList = res as Array<Monitor>;
+        this.resetForm();
+        this.toastr.success("Updated");
+      },
+      error: err => {
+        console.log(err);
+      }
     })
   }
 }
