aa42022e50402a5cdb154eef4e3f69f98dc297ca
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-06-01 17:57:05 +0200

    Add sorting feature to product pages

--- ComponentShopClient/src/app/shared/gpu.service.ts
+++ ComponentShopClient/src/app/shared/gpu.service.ts
@@ -106,6 +106,30 @@ export class GpuService {
     return this.http.get<any>(this.url + `/supportedProperties`, { withCredentials: true });
   }
 
+  sortAscending() {
+    this.gpuList.sort((m1, m2) => {
+      if (m1.price > m2.price) {
+        return 1;
+      }
+      if (m1.price < m2.price) {
+        return -1;
+      }
+      return 0;
+    })
+  }
+
+  sortDescending() {
+    this.gpuList.sort((m1, m2) => {
+      if (m1.price > m2.price) {
+        return -1;
+      }
+      if (m1.price < m2.price) {
+        return 1;
+      }
+      return 0;
+    })
+  }
+
   buildGetUri(): string {
     let uri: string = this.url + `/?currentPage=${this.currentPage}&pageSize=${this.pageSize}`;
     if (this.searchName != "") {
