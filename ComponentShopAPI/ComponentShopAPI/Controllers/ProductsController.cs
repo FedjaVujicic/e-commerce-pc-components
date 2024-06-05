ecbec39b76ecdebd7f621448a6aac814862bbb84
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-06-05 15:55:12 +0200

    Add Products Controller class

---
+++ ComponentShopAPI/ComponentShopAPI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using ComponentShopAPI.Helpers;
+using ComponentShopAPI.Models;
+using ComponentShopAPI.Services.Image;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComponentShopAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ComponentShopDBContext _context;
+        private readonly IImageService _imageService;
+
+        public ProductsController(ComponentShopDBContext context, IImageService imageService)
+        {
+            _context = context;
+            _imageService = imageService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(_context.Products.Select(product => new
+            {
+                product.Id,
+                product.Name,
+                product.Price,
+                availability = product.Quantity > 0,
+                ImageFile = _imageService.Download(product.ImageName, ProductType.Gpu)
+            }));
+        }
+
+    }
+}
--- ComponentShopAPI/ComponentShopAPI/Models/Product.cs
+++ ComponentShopAPI/ComponentShopAPI/Models/Product.cs
@@ -1,8 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ComponentShopAPI.Models
 {
+    [JsonDerivedType(typeof(Monitor))]
+    [JsonDerivedType(typeof(Gpu))]
     public abstract class Product
     {
         [Key]
