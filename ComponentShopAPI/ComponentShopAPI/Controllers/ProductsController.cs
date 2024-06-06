8404183f600236723268cd989c299c4bd1309e62
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-06-06 16:34:02 +0200

    Add pagination to GetProducts

--- ComponentShopAPI/ComponentShopAPI/Controllers/ProductsController.cs
+++ ComponentShopAPI/ComponentShopAPI/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using ComponentShopAPI.Helpers;
 using ComponentShopAPI.Models;
 using ComponentShopAPI.Services.Image;
+using ComponentShopAPI.Services.Pagination;
 using ComponentShopAPI.Services.ProductDtoFactory;
 using ComponentShopAPI.Services.Search;
 using Microsoft.AspNetCore.Mvc;
@@ -16,14 +17,16 @@ namespace ComponentShopAPI.Controllers
         private readonly IImageService _imageService;
         private readonly IProductDtoFactory _productDtoFactory;
         private readonly ISearchService _searchService;
+        private readonly IPaginationService _paginationService;
 
         public ProductsController(ComponentShopDBContext context, IImageService imageService,
-            IProductDtoFactory productDtoFactory, ISearchService searchService)
+            IProductDtoFactory productDtoFactory, ISearchService searchService, IPaginationService paginationService)
         {
             _context = context;
             _imageService = imageService;
             _productDtoFactory = productDtoFactory;
             _searchService = searchService;
+            _paginationService = paginationService;
         }
 
         [HttpGet]
@@ -37,6 +40,16 @@ namespace ComponentShopAPI.Controllers
                 _ => throw new BadHttpRequestException($"Invalid category {productQueryParameters.Category}")
             };
             products = _searchService.Search(products.ToList(), productQueryParameters);
+
+            Response.Headers.Append("Access-Control-Expose-Headers", "X-Total-Count");
+            Response.Headers.Append("X-Total-Count", products.Count().ToString());
+
+            if (products.Count() == 0)
+            {
+                return Ok();
+            }
+
+            products = _paginationService.Paginate(products.ToList(), productQueryParameters.CurrentPage, productQueryParameters.PageSize);
 
             var productDtos = products.ToList().Select(_productDtoFactory.Create).ToList();
             return Ok(productDtos);
