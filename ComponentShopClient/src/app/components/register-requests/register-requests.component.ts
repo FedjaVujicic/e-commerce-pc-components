8e65637106b41216778cdac42643ca980f791d82
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-07-16 16:13:06 +0200

    Add UI for register request approval

--- ComponentShopClient/src/app/components/register-requests/register-requests.component.ts
+++ ComponentShopClient/src/app/components/register-requests/register-requests.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { User } from '../../models/user';
+import { UserService } from '../../shared/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register-requests',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrl: './register-requests.component.css'
 })
 export class RegisterRequestsComponent {
+
+  users: Array<User> = [];
+
+  constructor(private http: HttpClient, public userService: UserService, private toastr: ToastrService) { }
+
+  ngOnInit() {
+    this.userService.getUsersForApproval().subscribe(res => {
+      this.users = res as Array<User>;
+    });
+  }
+
+  putUserStatus(username: string, status: string) {
+    this.userService.putUserStatus(username, status).subscribe(() => {
+      this.toastr.success("Success!");
+      this.userService.getUsersForApproval().subscribe(res => {
+        this.users = res as Array<User>;
+      });
+    });
+  }
 
 }
