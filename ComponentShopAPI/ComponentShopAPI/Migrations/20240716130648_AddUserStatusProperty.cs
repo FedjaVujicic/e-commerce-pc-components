02d8d76c4e99f922820c93ae7e6e1874cb9e0792
Fedja Vujicic <64490772+FedjaVujicic@users.noreply.github.com>
2024-07-16 15:12:48 +0200

    Add property Status to ApplicationUser

---
+++ ComponentShopAPI/ComponentShopAPI/Migrations/20240716130648_AddUserStatusProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComponentShopAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserStatusProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "AspNetUsers",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "AspNetUsers");
+        }
+    }
+}
